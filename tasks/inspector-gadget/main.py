de761af8c424cc582ce42bfba6ebf079a197b708
FrakenboK <xxxeromant@gmail.com>
2024-12-12 23:03:21 +0300

    Add pingers and inspector-gadjet

---
+++ tasks/inspector-gadget/main.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, render_template, jsonify
+from subprocess import run
+import json
+
+app = Flask(__name__)
+
+def create_resp(message: str) -> dict[str:str]:
+    return jsonify({"message": message})
+
+@app.route("/")
+def main():
+    return render_template("index.html")
+
+@app.route("/api/calculate", methods=['POST'])
+def pinger():
+    data = json.loads(request.data)
+    if "sign" not in data:
+        return create_resp("Nothing to calculate")
+    
+    answer = eval(data["sign"])
+
+    return create_resp(answer)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=61337, debug=False)
---
+++ tasks/pinger-deluxe-edition/main.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, render_template, jsonify
+from subprocess import run
+import json
+
+app = Flask(__name__)
+
+def create_resp(message: str) -> dict[str:str]:
+    return jsonify({"message": message})
+
+@app.route("/")
+def main():
+    return render_template("index.html")
+
+@app.route("/api/pinger", methods=['POST'])
+def pinger():
+    data = json.loads(request.data)
+    if "ip" not in data:
+        return create_resp("The IP address was not specified")
+    
+    data = run("ping %s -c 1" % data["ip"], capture_output=True, shell=True, text=True)
+    if data.stderr:
+        return create_resp("bebebe error")
+
+    return create_resp("bebebe pinged")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=51338, debug=False)
